bc147be32754f897b6415d7cb9fb7582fca0b5b8
Suyash Baoney <suyashbaoney58@gmail.com>
2025-11-28 22:59:27 +0530

    initial project setup

---
+++ SocketTalk/src/main/java/com/chat_application/SocketTalk/SocketTalkApplication.java
@@ -0,0 +1,13 @@
+package com.chat_application.SocketTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketTalkApplication.class, args);
+	}
+
+}
---
+++ SocketTalk/src/test/java/com/chat_application/SocketTalk/SocketTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_application.SocketTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
